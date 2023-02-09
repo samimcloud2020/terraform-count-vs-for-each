adff260eb3ab35d71b258fcb890f6fd456ddca72
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-02-09 22:16:20 +0530

    Update local-backend.tf

--- local-backend.tf
+++ local-backend.tf
@@ -1,8 +1,4 @@
 terraform {
-  required_version = ">= 0.12"
-}
-root@SDOP_Ts:~/terraform-modules/local# cat local.tf
-terraform {
   backend "local" {
     path = "/root/terraform-modules/local/terraform.tfstate"
   }
